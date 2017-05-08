f7605258b1f3d0f657a833759c4b008087ca44fe
dylanfisher <hi@dylanfisher.com>
2017-05-08 17:51:13 -0400

    Update theme author/description

--- functions/custom_image_sizes.php
+++ functions/custom_image_sizes.php
@@ -4,15 +4,15 @@
 // Custom image sizes
 //
 
-// function sandbox_set_default_image_sizes() {
-//   update_option( 'thumbnail_size_w', 160 );
-//   update_option( 'thumbnail_size_h', 160 );
-//   update_option( 'medium_size_w', 1000 );
-//   update_option( 'medium_size_h', 1000 );
-//   update_option( 'large_size_w', 2000 );
-//   update_option( 'large_size_h', 2000 );
-// }
-// add_action( 'after_switch_theme', 'sandbox_set_default_image_sizes' );
+function sandbox_set_default_image_sizes() {
+  update_option( 'thumbnail_size_w', 160 );
+  update_option( 'thumbnail_size_h', 160 );
+  update_option( 'medium_size_w', 1000 );
+  update_option( 'medium_size_h', 1000 );
+  update_option( 'large_size_w', 2000 );
+  update_option( 'large_size_h', 2000 );
+}
+add_action( 'after_switch_theme', 'sandbox_set_default_image_sizes' );
 
 // function sandbox_set_custom_image_sizes() {
 //   // Custom Image Sizes (Name, Width, Height, Hard Crop boolean)
