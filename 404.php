84d7c0bafd15e28904641a21b02f273449d2318e
dylanfisher <hi@dylanfisher.com>
2017-06-11 17:35:02 -0400

    Interlaced 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,11 @@
 <div class="content">
   <div class="four04">
-    <h2>404 - page not found</h2>
+    <div class="interlaced-logo-wrapper">
+      <div class="interlace">
+        <div class="interlace-item">
+          <div class="interlaced-logo">404 - page not found</div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
