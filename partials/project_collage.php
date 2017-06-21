ff2a92935c3f5d1fd2987fe8896698eedafdf97f
dylanfisher <hi@dylanfisher.com>
2017-06-20 22:47:24 -0400

    Add ability to change height of collage canvas

--- partials/project_collage.php
+++ partials/project_collage.php
@@ -1,4 +1,7 @@
-<?php $collage_data = json_decode ( get_field( 'collage' ) ); ?>
+<?php
+  $collage_data = json_decode ( get_field( 'collage' ), true );
+  $collage_height = isset( $collage_data['canvasHeightRatio'] ) ? $collage_data['canvasHeightRatio'] : 60;
+?>
 
 <?php ob_start(); ?>
   <a href="<?php the_permalink(); ?>" class="collage-link">
@@ -21,16 +24,16 @@
     <div class="project-collage__title-area project-collage__title-area--mobile col-xs-12">
       <?php echo $collage_description; ?>
     </div>
-    <div class="project-collage__media">
+    <div class="project-collage__media" style="height: <?php echo $collage_height; ?>vw;">
       <?php
         // var_dump($collage_data);
         $index = 0;
-        $collage_data_for_item = $collage_data->$index;
+        $collage_data_for_item = $collage_data['items'][$index];
 
-        $columns = $collage_data_for_item->columns;
-        $column_offset = $collage_data_for_item->columnOffset;
-        $vertical_offset = $collage_data_for_item->positionTop;
-        $z_index = $collage_data_for_item->zIndex;
+        $columns = $collage_data_for_item['columns'];
+        $column_offset = $collage_data_for_item['columnOffset'];
+        $vertical_offset = $collage_data_for_item['positionTop'];
+        $z_index = $collage_data_for_item['zIndex'];
       ?>
 
       <?php echo '<div class="project-collage__title-area project-collage__title-area--desktop project-collage-column col-xs-12 col-sm-' . $columns  . ' col-sm-offset-' . $column_offset . '" style="top: ' . $vertical_offset . '%; z-index: ' . $z_index . ';">'; ?>
@@ -42,12 +45,12 @@
           // Intentionally start the index at 1 because we add the custom description row first
           $index = 1;
           while ( have_rows( 'collage_items' ) ): the_row();
-            $collage_data_for_item = $collage_data->$index;
+            $collage_data_for_item = $collage_data['items'][$index];
 
-            $columns = $collage_data_for_item->columns;
-            $column_offset = $collage_data_for_item->columnOffset;
-            $vertical_offset = $collage_data_for_item->positionTop;
-            $z_index = $collage_data_for_item->zIndex;
+            $columns = $collage_data_for_item['columns'];
+            $column_offset = $collage_data_for_item['columnOffset'];
+            $vertical_offset = $collage_data_for_item['positionTop'];
+            $z_index = $collage_data_for_item['zIndex'];
 
             echo '<div class="project-collage-column project-collage-column--' . $index . ' col-xs-' . $columns . ' col-xs-offset-' . $column_offset . '" style="top: ' . $vertical_offset . '%; z-index: ' . $z_index . ';">';
               echo '<a href="' . get_permalink() . '" class="blank-link">';
