8e6e94c96a44cb5c6558c61e3b4f939c2a264156
dylanfisher <hi@dylanfisher.com>
2018-02-04 22:34:06 -0500

    Add LinkedIn and Facebook to footer

--- footer.php
+++ footer.php
@@ -12,6 +12,8 @@
           <div class="footer__social">
             <a href="https://twitter.com/midcomm" class="footer__social__item blank-link-hover" target="_blank">Twitter</a>
             <a href="https://www.instagram.com/midnightcommercial/" class="footer__social__item blank-link-hover" target="_blank">Instagram</a>
+            <a href="https://www.linkedin.com/company/midnight-commercial/" class="footer__social__item blank-link-hover" target="_blank">LinkedIn</a>
+            <a href="https://www.facebook.com/midnightcommercial/" class="footer__social__item blank-link-hover" target="_blank">Facebook</a>
           </div>
         </div>
       </footer>
